9e99c2b49abaaa755a5b72aa3d11cf78dac16e6c
Tahmim Ahmed Shibli <shibli05@gmail.com>
2018-05-21 01:01:39 +0600

    pipes example.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ArtistItemsComponent } from './artist-items/artist-items.component';
+import { SearchArtistsPipe } from './search-artists.pipe';
+import { ArtistDetailsComponent } from './artist-details/artist-details.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArtistItemsComponent
+    ArtistItemsComponent,
+    SearchArtistsPipe,
+    ArtistDetailsComponent
   ],
   imports: [
     BrowserModule,
